66eac7d5dc8569891b66fc54fb4240965590efdb
Levi Schouten <levi.schouten.werk@gmail.com>
2023-08-31 13:15:02 +0200

    fix: incorrect import

--- src/app/(home)/actions.ts
+++ src/app/(home)/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import z from "zod";
-import { schema } from "./_components/create-task";
 import { Task } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
+import { schema } from "./_components/task-form";
 
 export async function createTask(
   data: z.infer<typeof schema> & { userId: string }
